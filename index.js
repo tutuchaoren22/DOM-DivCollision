d093e9247dc1ee4794515860bf70953e32862888
duyuqian <15738381799@163.com>
2019-12-19 21:05:04 +0800

    [duyuqian]  fix:check and modify name of class and var

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     var yellowBox = document.getElementById("yellow-box");
     var greenBox = document.getElementById("green-box");
-
+    var areaBox = document.getElementsByClassName("area-box")[0];
     greenBox.onmousedown = function(e) {
         e = e || event;
         var posX = e.clientX - this.offsetLeft;
@@ -11,58 +11,55 @@ window.onload = function() {
             var left = e.clientX - posX;
             var top = e.clientY - posY;
             resetPosition(left, top, greenBox);
-            if (hasCollide(yellowBox, left, top, greenBox)) {
+            if (hasCollide(yellowBox, left, top)) {
                 yellowBox.style.background = 'blue';
             } else {
                 yellowBox.style.background = 'yellow';
             }
         };
-
         document.onmouseup = function() {
             document.onmousemove = null;
             document.onmouseup = null;
         }
     }
+
+    function resetPosition(left, top, greenBox) {
+        var width = areaBox.offsetWidth - yellowBox.offsetWidth;
+        var height = areaBox.offsetHeight - yellowBox.offsetHeight;
+        if ((0 < left && left <= width) && (0 < top && top <= height)) {
+            greenBox.style.left = left + 'px';
+            greenBox.style.top = top + 'px'
+        } else {
+            greenBox.style.left = (function() {
+                if (left > width) {
+                    return width + 'px';
+                } else if (left < 0) {
+                    return 0 + 'px';
+                } else {
+                    return left + 'px';
+                }
+            }());
+            greenBox.style.top = (function() {
+                if (top > height) {
+                    return height + 'px';
+                } else if (top < 0) {
+                    return 0 + 'px';
+                } else {
+                    return top + 'px';
+                }
+            }());
+        }
+    }
+
+    function hasCollide(staticBox, left, top) {
+        var l = staticBox.offsetLeft - staticBox.offsetWidth;
+        var t = staticBox.offsetTop - staticBox.offsetHeight;
+        var r = staticBox.offsetLeft + staticBox.offsetWidth;
+        var b = staticBox.offsetTop + staticBox.offsetHeight;
+        if (left < l || top < t || left > r || top > b) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 };
-
-function resetPosition(left, top, greenBox) {
-    if ((0 < left && left <= 920) && (0 < top && top <= 720)) {
-        greenBox.style.left = left + 'px';
-        greenBox.style.top = top + 'px'
-    } else {
-        greenBox.style.left = (function() {
-            if (left > 920) {
-                return 920 + 'px';
-            } else if (left < 0) {
-                return 0 + 'px';
-            } else {
-                return left + 'px';
-            }
-        }());
-        greenBox.style.top = (function() {
-            if (top > 720) {
-                return 720 + 'px';
-            } else if (top < 0) {
-                return 0 + 'px';
-            } else {
-                return top + 'px';
-            }
-        }());
-    }
-}
-
-function hasCollide(obj, left, top, obj1) {
-    var l1 = obj.offsetLeft - obj.offsetWidth;
-    var t1 = obj.offsetTop - obj.offsetHeight;
-    var r1 = obj.offsetLeft + obj.offsetWidth;
-    var b1 = obj.offsetTop + obj.offsetHeight;
-    if (left < l1 || top < t1 || left > r1 || top > b1) {
-        // obj.style.zIndex = 3;
-        // obj1.style.zIndex = 1;
-        return false;
-    } else {
-        // obj.style.zIndex = 1;
-        // obj1.style.zIndex = 3;
-        return true;
-    }
-}
